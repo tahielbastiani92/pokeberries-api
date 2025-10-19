421216fde8d423e6658d6be7fa79ce88854a3548
tahielbastiani92 <bastianitahiel@gmail.com>
2025-10-19 01:42:58 -0300

    feat: endpoint berries stats

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Response
+from src.services.pokeapi import get_all_berries_data
+from src.utils.statistics import calculate_statistics
+from typing import List, Dict
 
 app = FastAPI(title="Pokeberries API")
 
@@ -7,3 +9,17 @@ app = FastAPI(title="Pokeberries API")
 @app.get("/")
 def intial():
     return {"message": "Working.. Pokeberries statistics is coming.."}
+
+@app.get("/allBerryStats", response_model=dict)
+def all_berry_statstics(response: Response):
+    """
+    Returns berries' statistics
+    """
+    try:
+        berries_data = get_all_berries_data()
+        stats = calculate_statistics(berries_data)
+        response.headers["Content-Type"] = "application/json"
+        return stats
+    except Exception as e:
+        print(f"Error: {e}")
+        return {"error": "Unexpected error occurred."}
